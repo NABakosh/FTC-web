d1d10995599328a21767614b7937720f4e787ac4
NABako <goldongame.06@gmail.com>
2024-11-20 16:32:13 +0500

    'check'

--- script/script.js
+++ script/script.js
@@ -1,2 +1,10 @@
 const width = screen.width
 const height = screen.height
+const connect = document.getElementById("connect")
+const mira1 = document.getElementsById("mira1")
+const mira2 = document.getElementsById("mira2")
+if(width < 500){
+    connect.style.display = 'none';
+    mira1.style.display = 'none';
+    mira2.style.display = 'flex';
+}
