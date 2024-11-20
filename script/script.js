f8d222d70e108a96ad81cfff7e5b50b08d4da952
NABako <goldongame.06@gmail.com>
2024-11-20 17:01:03 +0500

    'check'

--- script/script.js
+++ script/script.js
@@ -1,10 +1,22 @@
 const width = screen.width
 const height = screen.height
-const connect = document.getElementById("connect")
-const mira1 = document.getElementsById("mira1")
-const mira2 = document.getElementsById("mira2")
+
+const connects = document.querySelectorAll(".connect")
+const mira1 = document.getElementById("mira1")
+const mira2 = document.getElementById("mira2")
+
 if(width < 500){
-    connect.style.display = 'none';
-    mira1.style.display = 'none';
     mira2.style.display = 'flex';
+    connects.forEach(connect => {
+        connect.style.display = 'none'
+    })
+    mira1.style.display = 'none';
 }
+else{
+    mira2.style.display = 'none';
+    connect.forEach(connect => {
+        connects.style.display = 'flex'
+    })
+    mira1.style.display = 'flex';
+}
+console.log(width)
